09e5698428cf94921f32ef012f89b42ebb8d3917
Min <113046771+minnemoinguoi@users.noreply.github.com>
2024-03-26 12:29:05 +0700

    Delete python directory

--- python/b_t2.py
+++
@@ -1,8 +0,0 @@
-def gt(n):
-    giai_thua = 1 
-    for i in range(1, n+1):
-        giai_thua = giai_thua * i
-    return giai_thua
-
-n =  int(input("n = "))
-print("giai thua cua ",n," la ", gt(n))
